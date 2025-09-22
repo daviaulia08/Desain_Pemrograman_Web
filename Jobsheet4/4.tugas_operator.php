fe23ddd2ed02d15a6118732bf36ce700f3159ff8
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-09-22 23:44:53 +0700

    prak 3 soal 3.6

---
+++ Jobsheet4/4.tugas_operator.php
@@ -0,0 +1,13 @@
+<?php
+// prak 3 soal 3.6
+$totalKursi = 45;
+$kursiTerisi = 28;
+
+$kursiKosong = $totalKursi - $kursiTerisi;
+$persentaseKosong = ($kursiKosong / $totalKursi) * 100;
+
+echo "Total Kursi = $totalKursi <br>";
+echo "Kursi Terisi = $kursiTerisi <br>";
+echo "Kursi Kosong = $kursiKosong <br>";
+echo "Persentase Kursi Kosong = $persentaseKosong %";
+?>
