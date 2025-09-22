1b9616fae1751c5c3d19a27976dd14e6c7183185
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-09-22 23:21:46 +0700

    prak 3 soal 3.2

--- Jobsheet4/3.operator.php
+++ Jobsheet4/3.operator.php
@@ -18,4 +18,22 @@ echo "hasil a / b = {$hasilBagi} <br>";
 echo "hasil a % b = {$sisaBagi} <br>";
 echo "hasil a ^ b = {$pangkat} <br>";
 
+echo "<br>";
+
+// Operator Perbandingan
+$hasilSama            = $a == $b;
+$hasilTidakSama       = $a != $b;
+$hasilLebihKecil      = $a < $b;
+$hasilLebihBesar      = $a > $b;
+$hasilLebihKecilSama  = $a <= $b;
+$hasilLebihBesarSama  = $a >= $b;
+
+echo "hasil a == b = {$hasilSama} <br>";
+echo "hasil a != b = {$hasilTidakSama} <br>";
+echo "hasil a < b = {$hasilLebihKecil} <br>";
+echo "hasil a > b = {$hasilLebihBesar} <br>";
+echo "hasil a <= b = {$hasilLebihKecilSama} <br>";
+echo "hasil a >= b = {$hasilLebihBesarSama} <br>";
+
+echo "<br>";
 ?>
