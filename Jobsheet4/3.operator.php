141c9ba25a05cd80ee916f23f7075e12a4dc433d
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-09-22 23:16:23 +0700

    prak 3 soal 3.1

---
+++ Jobsheet4/3.operator.php
@@ -0,0 +1,21 @@
+<?php
+
+//operator aritmatika
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali   = $a * $b;
+$hasilBagi   = $a / $b;
+$sisaBagi    = $a % $b;
+$pangkat     = $a ** $b;
+
+echo "hasil a + b = {$hasilTambah} <br>";
+echo "hasil a - b = {$hasilKurang} <br>";
+echo "hasil a x b = {$hasilKali} <br>";
+echo "hasil a / b = {$hasilBagi} <br>";
+echo "hasil a % b = {$sisaBagi} <br>";
+echo "hasil a ^ b = {$pangkat} <br>";
+
+?>
