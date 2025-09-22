9a68aa83be7bab67f9c923050796f73e2505dfe4
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-09-23 01:00:46 +0700

    prak 4 soal 4.8'

---
+++ Jobsheet4/8.tugas_skor_game.php
@@ -0,0 +1,18 @@
+<?php
+$skorPemain = [550, 420]; // pemain 1 = 550, pemain 2 = 420
+
+$jumlahPemain = 0;
+foreach ($skorPemain as $s) {
+    $jumlahPemain++;
+}
+
+for ($i = 0; $i < $jumlahPemain; $i++) {
+    echo "Total skor pemain " . ($i + 1) . " adalah: " . $skorPemain[$i] . "<br>";
+    
+    if ($skorPemain[$i] > 500) {
+        echo "Apakah pemain mendapatkan hadiah tambahan? YA<br><br>";
+    } else {
+        echo "Apakah pemain mendapatkan hadiah tambahan? TIDAK<br><br>";
+    }
+}
+?>
