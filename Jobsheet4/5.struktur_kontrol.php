0b93221c016b7daa71ebaa55f354c4c64a40ad64
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-09-22 23:48:23 +0700

    prak 4 soal 4.1

---
+++ Jobsheet4/5.struktur_kontrol.php
@@ -0,0 +1,16 @@
+<?php
+// if else if (Nilai Huruf)
+$nilaiNumerik = 92;
+
+if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
+    echo "Nilai huruf: A <br>";
+} elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90) {
+    echo "Nilai huruf: B <br>";
+} elseif ($nilaiNumerik >= 70 && $nilaiNumerik < 80) {
+    echo "Nilai huruf: C <br>";
+} elseif ($nilaiNumerik < 70) {
+    echo "Nilai huruf: D <br>";
+}
+
+echo "<br>";
+?>
