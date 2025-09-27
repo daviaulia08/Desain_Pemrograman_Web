1929d0819b0d8533ff5cfac9b7688c4202432fef
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-09-28 02:56:26 +0700

    prak 1 soal 1

---
+++ Jobsheet5/1.array_1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+
+        <?php
+            $listDosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+            echo $listDosen[2] , "<br>";
+            echo $listDosen[0] , "<br>";
+            echo $listDosen[1] , "<br>";
+        ?>
+    
+    <body>
+</html>
