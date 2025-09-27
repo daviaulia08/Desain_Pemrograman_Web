45c99153cd53d3a02a5b0ea94f5df1639eba4930
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-09-28 03:45:33 +0700

    prak 4 soal 4

---
+++ Jobsheet5/5.function.php
@@ -0,0 +1,13 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Davi <br/>";
+    echo "Senang berkenalan dengan Anda <br/>";
+}
+
+//memanggil fungsi
+perkenalan();
+perkenalan();
+
+?>
