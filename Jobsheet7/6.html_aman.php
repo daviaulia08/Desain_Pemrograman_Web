fa73467bf8511ec00adf387dee97b94965847d48
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-10-18 00:08:41 +0700

    prak 4 soal 4.2

--- Jobsheet7/6.html_aman.php
+++ Jobsheet7/6.html_aman.php
@@ -2,9 +2,17 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $input = $_POST['input'];
         $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+        $email = $_POST['email'];
 
         echo "<h3>Hasil Input Aman:</h3>";
         echo "<p>$input</p>";
+
+        // memeriksa apakah input adalah email yang valid
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "<p>Email: $email (valid)</p>";
+        } else {
+            echo "<p>Email tidak valid!</p>";
+        }
     }
 ?>
 
@@ -16,7 +24,9 @@
 <body>
     <form method="post" action="">
         <label>Masukkan teks:</label><br>
-        <input type="text" name="input" required>
+        <input type="text" name="input" required><br>
+        <label>Masukkan email:</label><br>
+        <input type="email" name="email" required><br><br>
         <button type="submit">Kirim</button>
     </form>
 </body>
