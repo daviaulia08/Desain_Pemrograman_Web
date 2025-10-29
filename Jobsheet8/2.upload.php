e005381b1f016f4b4aeb8d5d2a62b5f9564c6dee
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-10-29 11:34:20 +0700

    prak 1 soal 6

--- Jobsheet8/2.upload.php
+++ Jobsheet8/2.upload.php
@@ -5,18 +5,13 @@ if (isset($_POST["submit"])) {
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
     // nambahin ekstensi gambar ke daftar yang diizinkan
-    $allowedExtensions = array("txt", "pdf", "doc", "docx", "jpg", "jpeg", "png");
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
     $maxsize = 3 * 1024 * 1024;
 
-    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) 
+    {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah.<br>";
-
-            // jika file adalah gambar, nampilin pratinjau
-            if (in_array($fileType, array("jpg", "jpeg", "png"))) {
-                echo "<p>Pratinjau Gambar:</p>";
-                echo "<img src='$targetfile' width='200' style='height:auto; border:1px solid #ccc; padding:5px;'>";
-            }
         } else {
             echo "Gagal mengunggah file.";
         }
