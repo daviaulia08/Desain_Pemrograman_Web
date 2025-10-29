1da63735b437503ab34219db5fb0a023bda999a0
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2025-10-29 10:56:55 +0700

    prak 1 soal 1

---
+++ Jobsheet8/1.form_upload.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>File Upload</title>
+</head>
+<body>
+    <form action="2.upload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="myfile">
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
